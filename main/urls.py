a284d6c90551172e1cb77600fcdc5ec90ae216cf
unknown <rikenmaharjan4@gmail.com>
2025-12-31 23:57:49 +0545

    12/31

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('', views.home, name='home'), 
+        path('', views.home1, name='home1'),
+        path('about/', views.about, name='about'), 
+        path('contact/', views.contact, name='contact'), 
+        path('farmers/', views.farmers, name='farmers'), 
+        path('products/', views.products, name='products'), 
+        path('products_detail/', views.products_detail, name='products_detail'), 
+        path('vendor/', views.vendor, name='vendor'), 
+
+]
---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'pages/home.html') 
+
+def home1(request):
+    return render(request, 'pages/home1.html') 
+
+def about(request):
+    return render(request, 'pages/about.html') 
+
+def contact(request):
+    return render(request, 'pages/contact.html') 
+
+def farmers(request):
+    return render(request, 'pages/farmers.html') 
+
+def products_detail(request):
+    return render(request, 'pages/products_detail.html') 
+
+def products(request):
+    return render(request, 'pages/products.html') 
+
+def vendor(request):
+    return render(request, 'pages/vendor.html') 
