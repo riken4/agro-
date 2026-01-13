70dff42525195daeb580d56789e412789a42b0a5
unknown <rikenmaharjan4@gmail.com>
2026-01-13 13:22:18 +0545

    1/13

--- agroEcommerce/urls.py
+++ agroEcommerce/urls.py
@@ -24,16 +24,32 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home_page, name='home_page'),
     
-    path('',views.login_page,name='login_page'),
+    path('login/',views.login_page,name='login_page'),
     path('logout/',views.logout_page,name='logout'),
     path('admin-dashboard/',include('adminpanel.urls')),
     path('vendor-dashboard/',include('vendor.urls')),
     path('farmer-dashboard/',include('farmer.urls')),
     
-    
+      path('', views.home, name='home'), 
+        path('about/', views.about, name='about'), 
+        path('contact/', views.contact, name='contact'), 
+        path('farmers/', views.farmers, name='farmers'), 
+        path('products/', views.products, name='products'), 
+        path('products_detail/', views.products_detail, name='products_detail'), 
+        path('vendor/', views.vendor, name='vendor'), 
+        path('cart/', views.cart, name='cart'), 
+        path('vendor_detail/', views.vendor_detail, name='vendor_detail'), 
+        path('checkout/', views.checkout, name='checkout'), 
+        path('vegetables/', views.vegetables, name='vegetables'), 
+        path('farmers_detail/', views.farmers_detail, name='farmers_detail'), 
+        path('order_confirmation/', views.order_confirmation, name='order_confirmation'), 
+        path('privacy_policy/', views.privacy_policy, name='privacy_policy'), 
+        path('faq/', views.faq, name='faq'),
+        path('term_of_services/', views.term_of_services, name='term_of_services'),
+        path('refund_and_return/', views.refund_and_return, name='refund_and_return'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- agroEcommerce/views.py
+++ agroEcommerce/views.py
@@ -9,8 +9,6 @@ from .models import UserProfile,UserRole
 
 
 
-def home_page(request):
-    return render(request,'website/pages/home.html')
 
 
 def login_page(request):
@@ -43,3 +41,58 @@ def logout_page(request):
     messages.success(request,'Logged out successfully!')
     return redirect('login_page')
 
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'website/pages/home.html') 
+
+def home1(request):
+    return render(request, 'website/pages/home1.html') 
+
+def about(request):
+    return render(request, 'website/pages/about.html') 
+
+def contact(request):
+    return render(request, 'website/pages/contact.html') 
+
+def farmers(request):
+    return render(request, 'website/pages/farmers.html') 
+
+def products_detail(request):
+    return render(request, 'website/pages/products_detail.html') 
+
+def products(request):
+    return render(request, 'website/pages/products.html') 
+
+def vendor(request):
+    return render(request, 'website/pages/vendor.html') 
+
+def cart(request):
+    return render(request, 'website/pages/cart.html') 
+
+def vendor_detail(request):
+    return render(request, 'website/pages/vendor_detail.html') 
+
+def checkout(request):
+    return render(request, 'website/pages/checkout.html') 
+
+def vegetables(request):
+    return render(request, 'website/pages/vegetables.html') 
+
+def farmers_detail(request):
+    return render(request, 'website/pages/farmers_detail.html') 
+
+def order_confirmation(request):
+    return render(request, 'website/pages/order_confirmation.html') 
+
+def faq(request):
+    return render(request, 'website/pages/faq.html')
+def privacy_policy(request):
+    return render(request, 'website/pages/privacy_policy.html')
+
+def term_of_services(request):
+    return render(request, 'website/pages/term_of_services.html')
+
+def refund_and_return(request):
+    return render(request, 'website/pages/refund_and_return.html')
